12ec2e357270ca082fbe55ed040eb93b47e972e6
Janaina <tjanaina92@gmail.com>
2023-02-23 14:01:44 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -23,21 +27,29 @@ public class UserResource {
 	 * retornar a resposta com sucesso no http). E então chamamos o .body para
 	 * retornar ao corpo da resposta esse usuário que foi instanciado
 	 */
-	
+
 	@Autowired
-	private UserService service; 
-	
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		//User u = new User(1L, "Maria", "maria@gmail.com","9999999", "12345");
+	public ResponseEntity<List<User>> findAll() {
+		// User u = new User(1L, "Maria", "maria@gmail.com","9999999", "12345");
 		List<User> list = service.findAll();
 		return ResponseEntity.ok(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
 }
